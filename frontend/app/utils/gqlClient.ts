056ed6d3febd60ee6d747cfd1241ccbd219dd08c
flohop <florian.hoppe@kertos.io>
2023-05-30 10:07:57 +0200

    running out of messages...

--- frontend/app/utils/gqlClient.ts
+++ frontend/app/utils/gqlClient.ts
@@ -3,8 +3,8 @@ import {ApolloClient, HttpLink, InMemoryCache} from "@apollo/client";
 let client: ApolloClient<any> | null = null;
 
 
-const URI = "http://localhost:4000/graphql"
-//const URI = "http://141.95.127.73:4000/graphql"
+//const URI = "http://localhost:4000/graphql"
+const URI = "http://141.95.127.73:4000/graphql"
 export const getClient = () => {
     // create a new client if there's no existing one
     // or if we are running on the server.
